9ce5b4f5292455e68e0761f89a4a5ca2d3a573b9
VincenzoAmari <vincenzo.am99@gmail.com>
2025-06-10 12:11:49 +0200

    snack 1

--- snack.js
+++ snack.js
@@ -1,1 +1,8 @@
-module.exports = {};
+function getInitials(nomeCompleto) {
+  const [nome, cognome] = nomeCompleto.split(" ");
+  return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}.`;
+}
+
+module.exports = {
+  getInitials,
+};
--- snack.test.js
+++ snack.test.js
@@ -1,3 +1,6 @@
-const {} = require("./snack.js");
+const { getInitials } = require("./snack.js");
 
-test("la funzione getInitials restituisce le iniziali di un nome completo", () => {});
+test("la funzione getInitials restituisce le iniziali di un nome completo", () => {
+  expect(getInitials("Vincenzo Amari")).toBe("V.A.");
+  expect(getInitials("federica giorgini")).toBe("F.G.");
+});
